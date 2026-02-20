9ba16663a88f84fd99e437eda8a577b9f06d576a
jripke74 <jripke74@gmail.com>
2026-02-19 17:22:34 -0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Database connection
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Required for Heroku [7]
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/submit', async (req, res) => {
+  const { title, releaseyeardvd, releaseyearmovie } = req.body;
+  try {
+    await pool.query('INSERT INTO movies (title, releaseyeardvd, releaseyearmovie) VALUES ($1, $2, $3)', [title, releaseyeardvd, releaseyearmovie]);
+    res.send('Data stored successfully!');
+  } catch (err) {
+    res.status(500).send('Error: ' + err.message);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
